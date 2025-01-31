c5949305aee2d819a28814e84f1ccc022ec79582
sjMun09 <ohoh7391@naver.com>
2025-01-31 15:14:19 +0900

    feat: 초기 세팅

---
+++ src/main/java/com/example/scheduleApp/ScheduleAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.scheduleApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleAppApplication.class, args);
+	}
+
+}
